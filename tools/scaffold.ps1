67be14eb618a8a814f697a9a0e617be5443e4d33
snowinmars <snowinmars@yandex.ru>
2019-03-28 13:38:00 +0300

    Refactored.

--- tools/scaffold.ps1
+++ tools/scaffold.ps1
@@ -1,12 +1,13 @@
 function main {
-param([string]$rawTemplates, [string]$rawGenerated)
+param([string]$rawTemplates, [string]$rawCsGenerated, [string]$rawSqlGenerated)
     $root = $pwd.Path + '\..\'
     $astyleExePath = $root + "tools\astyle.exe"
     # TODO [sim]:  move tt executable to tools folder
     $textTransformExePath = "D:\Program Files (x86)\Microsoft Visual Studio\2017\Community\Common7\IDE\TextTransform.exe"
 
     $templates = $rawTemplates.Split(';')
-    $generates = $rawGenerated.Split(';')
+    $csGenerates = $rawCsGenerated.Split(';')
+    $sqlGenerates = $rawSqlGenerated.Split(';')
 
     $templates | % {
         $template = $_
@@ -17,17 +18,29 @@ param([string]$rawTemplates, [string]$rawGenerated)
         Write-Host 'Transformed ' $template
     }
 
-    $generates | % {
-        $generated = $_
+    $csGenerates | % {
+        $csGenerated = $_
         
         # fix indents
-        Invoke-Expression -Command ($astyleExePath + ' --style=allman --indent-namespaces ' + $generated)
+        Invoke-Expression -Command ($astyleExePath + ' --style=allman --indent-namespaces ' + $csGenerated)
 
         # remove empty lines (this is not possible with current astyle version)
-        $value = Get-Content $generated | Select-String -NotMatch -Pattern ^\n*$
+        $value = Get-Content $csGenerated | Select-String -NotMatch -Pattern ^\n*$
 
-        Set-Content -Path $generated -Value $value
+        Set-Content -Path $csGenerated -Value $value
+    }
+
+    $sqlGenerates | % {
+        $sqlGenerated = $_
+        
+        # fix indents
+        Invoke-Expression -Command ($astyleExePath + ' --style=google ' + $sqlGenerated)
+
+        # remove empty lines (this is not possible with current astyle version)
+        $value = Get-Content $sqlGenerated | Select-String -NotMatch -Pattern ^\n*$
+
+        Set-Content -Path $sqlGenerated -Value $value   
     }
 }
 
-main $args[0] $args[1]
+main $args[0] $args[1] $args[2]
